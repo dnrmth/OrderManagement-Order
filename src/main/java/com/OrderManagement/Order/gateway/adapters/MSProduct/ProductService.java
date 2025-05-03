435ac8a635335739d6c3d83e843f8ad08ac3e67d
Denner Matheus <dennermat@gmail.com>
2025-05-03 06:50:54 -0300

    forgot product to set fallback class

--- src/main/java/com/OrderManagement/Order/gateway/adapters/MSProduct/ProductService.java
+++ src/main/java/com/OrderManagement/Order/gateway/adapters/MSProduct/ProductService.java
@@ -1,12 +1,15 @@
 package com.OrderManagement.Order.gateway.adapters.MSProduct;
 
 import com.OrderManagement.Order.gateway.adapters.MSProduct.dto.ProductServiceDto;
+import com.OrderManagement.Order.gateway.adapters.MSProduct.fallback.ProductServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ProductService", url = "http://localhost:8084/product")
+@FeignClient(name = "ProductService",
+        url = "http://localhost:8084/product",
+        fallback = ProductServiceFallback.class)
 public interface ProductService {
 
     @GetMapping(value = "/{SKU}")
